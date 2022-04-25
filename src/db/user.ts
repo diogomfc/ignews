3a6674fb966674fdc33f424113e614899cc36c60
diogomfc <diogomfc@hotmail.com>
2022-04-25 16:05:47 -0300

    api

---
+++ src/db/user.ts
@@ -0,0 +1,5 @@
+export const Users = [
+  { id: 1, nome: 'Diogo' },
+  { id: 2, nome: 'Davi' },
+  { id: 3, nome: 'Gabrielly' },
+];
---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+//import {Users} from '../../db/user';
+
+// JWT (Storage)
+// Next Auth (Social Login)
+// Cognito, Auth0
+
+
+const getUsers = async (req: NextApiRequest, res: NextApiResponse) => {
+  //const users = await Users;
+  
+  const users = await [
+    {id: 1, nome: 'Diogo'},
+    {id: 2, nome: 'Davi'},
+    {id: 3, nome: 'Gabi'},
+  ];
+
+  return res.json(users);
+};
+
+export default getUsers;
